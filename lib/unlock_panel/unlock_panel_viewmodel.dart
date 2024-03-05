225688e0d63c02c71be78d0819a631e3c6cee002
Kristifor Milchev <km@airzen.io>
2024-03-05 16:06:49 +0200

    Adding a subscriber to the wake up event after the app has been brought back to the foreground.

--- lib/unlock_panel/unlock_panel_viewmodel.dart
+++ lib/unlock_panel/unlock_panel_viewmodel.dart
@@ -3,6 +3,8 @@ import 'package:components/pin_code_panel/pin_code_panel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:infrastructure/interfaces/iauthorization_service.dart';
+import 'package:infrastructure/interfaces/iobserver.dart';
+import 'package:infrastructure/interfaces/ipage_router_service.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:shared/page_view_model.dart';
 import 'package:local_auth/error_codes.dart' as auth_error;
@@ -13,12 +15,17 @@ import 'package:components/fill_totp_code/fill_totp_code.dart';
 class UnlockPanelViewModel extends PageViewModel {
   late IAuthorizationService _authorizationService;
   final LocalAuthentication auth = LocalAuthentication();
+  late IPageRouterService _routerService;
+  late IObserver _observer;
 
   Widget _lockOption = const Column();
   Widget get lockOption => _lockOption;
 
   UnlockPanelViewModel(super.context) {
     _authorizationService = getIt.get<IAuthorizationService>();
+    _routerService = getIt.get<IPageRouterService>();
+    _observer = getIt.get<IObserver>();
+    _observer.subscribe("woken-up", ready);
   }
 
   ready() async {
@@ -40,11 +47,7 @@ class UnlockPanelViewModel extends PageViewModel {
         Future.delayed(
           const Duration(milliseconds: 500),
           () {
-            router.changePage(
-              "/setup-lock",
-              pageContext,
-              TransitionData(next: PageTransition.slideForward),
-            );
+            router.router.router.go("/setup-lock");
           },
         );
         break;
@@ -60,6 +63,8 @@ class UnlockPanelViewModel extends PageViewModel {
       );
 
       if (result) {
+        _routerService.isLocked = false;
+
         router.changePage(
           "/passwords",
           // ignore: use_build_context_synchronously
@@ -68,6 +73,8 @@ class UnlockPanelViewModel extends PageViewModel {
         );
       }
     } on PlatformException catch (e) {
+      await auth.stopAuthentication();
+      await requestBiometric();
       if (e.code == auth_error.notEnrolled) {
         // Add handling of no hardware here.
       } else if (e.code == auth_error.lockedOut ||
@@ -77,5 +84,12 @@ class UnlockPanelViewModel extends PageViewModel {
         // ...
       }
     }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _observer.dispose("woken-up");
+    super.dispose();
   }
 }
